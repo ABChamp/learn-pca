00f18916526b7d4e256748bd483dc046b397baf7
56160232 <56160232@go.buu.ac.th>
2018-08-07 20:48:33 +0700

    full code plotly

--- plotly/main.py
+++ plotly/main.py
@@ -54,7 +54,7 @@ plotly.offline.plot(fig) """
 # the covariance matrix depends on the measurement scales of the original features
 # transformation of the data onto unit scale (mean=0 and variance=1)
 X_std = StandardScaler().fit_transform(X)
-mean_vec = np.mean(X_std, axis=0)
+# mean_vec = np.mean(X_std, axis=0)
 # cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
 cov_mat = np.cov(X_std.T)
 eig_vals, eig_vecs = np.linalg.eig(cov_mat)
@@ -64,3 +64,46 @@ print('Eigenvalues \n%s' % eig_vals)
 
 for ev in eig_vecs:
     np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eig_pairs.sort()
+eig_pairs.reverse()
+
+# Visually confirm that the list is correctly sorted by decreasing eigenvalues
+print('Eigenvalues in descending order:')
+for i in eig_pairs:
+    print(i[0])
+
+matrix_w = np.hstack((eig_pairs[0][1].reshape(4,1),
+                      eig_pairs[1][1].reshape(4,1)))
+
+# print('Matrix W:\n', matrix_w)
+Y = X_std.dot(matrix_w)
+traces = []
+
+for name in ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'):
+
+    trace = Scatter(
+        x=Y[y==name,0],
+        y=Y[y==name,1],
+        mode='markers',
+        name=name,
+        marker=Marker(
+            size=12,
+            line=Line(
+                color='rgba(217, 217, 217, 0.14)',
+                width=0.5),
+            opacity=0.8))
+    traces.append(trace)
+
+
+data = Data(traces)
+layout = Layout(showlegend=True,
+                scene=Scene(xaxis=XAxis(title='PC1'),
+                yaxis=YAxis(title='PC2'),))
+
+fig = Figure(data=data, layout=layout)
+plotly.offline.plot(fig)
